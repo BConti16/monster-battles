c79665eff5a67a49eb8fcac9579b3de0acf05e02
Brett Conti <baconti16@gmail.com>
2020-09-18 18:14:28 -0400

    added monstergenerator test code to main

--- main/main.cpp
+++ main/main.cpp
@@ -1,23 +1,37 @@
 #include<iostream>
+#include<string>
+#include<ctime>
+#include<random>
 #include "weapon.h"
 #include "monster.h"
+#include "monstergenerator.h"
 
 void Pause()
 {
-	std::cout << "Press any key to continue... ";
-	char ans{};
-	std::cin >> ans;
+	std::cout << "\nPress enter to continue... ";
+	std::string ans{};
+	std::getline(std::cin, ans);
 }
 
 int main()
 {
+	//Define and seed random number generator
+	std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+
 	Weapon club{ Weapon::Type::club };
 	club.print();
 
 	std::cout << '\n';
 
+	std::cout << "Manual Monster\n";
 	Monster demon{ Monster::Type::demon, "Dracula", Weapon::Type::claws, 600.0 };
 	demon.print();
+
+	std::cout << '\n';
+
+	std::cout << "Random Monster\n";
+	Monster m{ MonsterGenerator::generateRandomMonster(mt) };
+	m.print();
 
 	Pause();
 	return 0;
