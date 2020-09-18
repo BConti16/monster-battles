9747d1a368a77137a70f0b5f4a4b0d6add44c513
Brett Conti <baconti16@gmail.com>
2020-09-18 16:41:18 -0400

    Added monster test code to main

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include "weapon.h"
+#include "monster.h"
 
 void Pause()
 {
-	std::cout << "Press any key to continue...";
+	std::cout << "Press any key to continue... ";
 	char ans{};
 	std::cin >> ans;
 }
@@ -12,6 +13,11 @@ int main()
 {
 	Weapon club{ Weapon::Type::club };
 	club.print();
+
+	std::cout << '\n';
+
+	Monster demon{ Monster::Type::demon, "Dracula", Weapon::Type::claws, 600.0 };
+	demon.print();
 
 	Pause();
 	return 0;
