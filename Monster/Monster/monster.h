746bf6a0d31af64e12815313682a12261683b2c1
Brett Conti <baconti16@gmail.com>
2020-09-18 16:35:06 -0400

    cleaned up monster.h

--- Monster/Monster/monster.h
+++ Monster/Monster/monster.h
@@ -19,18 +19,20 @@ public:
 
 private:
 	Type m_type;
-	Weapon m_weapon;
 	std::string m_name;
+	Weapon m_weapon;
 	double m_hp;
 
 public:
-	Monster(const Type& type, const Weapon::Type& weapon, const std::string& name, double hp) : m_type{ type }, m_weapon{ Weapon(weapon) }, m_name{ name }
+	Monster(const Type& type, const std::string& name, const Weapon::Type& weapon, double hp) : m_type{ type }, m_name{ name }, m_weapon{ Weapon(weapon) }
 	{
 		if (hp <= 0.0)
-			m_hp = 100.0;
+			m_hp = 650.0; //default health if invalid amount passed in
 		else
 			m_hp = hp;
 	}
+
+	~Monster() = default;
 	
 	const std::string getTypeString() const;
 	const std::string& getName() const;
