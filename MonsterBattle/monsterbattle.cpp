45e50931e8f10829cb4df72c55420a83e1912d2f
Brett Conti <baconti16@gmail.com>
2020-09-20 18:49:03 -0400

    added printWinner member function to print the battle's winner

--- MonsterBattle/monsterbattle.cpp
+++ MonsterBattle/monsterbattle.cpp
@@ -3,13 +3,22 @@
 #include "monstergenerator.h"
 #include<iostream>
 #include<random>
+#include<string>
 
-void MonsterBattle::beginMessage(Monster& m1, Monster& m2)
+void MonsterBattle::beginMessage(const Monster& m1, const Monster& m2)
 {
 	std::cout << "Introducing our two combatants:\n";
 	std::cout << "\nCombatant One: "; m1.print();
 	std::cout << "Combatant Two: "; m2.print();
 	std::cout << "\nLet the battle commence!\n";
+}
+
+void MonsterBattle::printWinner(const Monster& m1, const Monster& m2)
+{
+	//Generate the victory message based on whether or not m1 is dead
+	//if m1 is dead, m2 is the victor, if not, m1 is the victor
+	std::string victoryMessage{ (m1.isDead()) ? "\nCombatant Two: " + m2.getName() + " is the victor!\n" : "\nCombatant One: " + m1.getName() + " is the victor!\n" };
+	std::cout << victoryMessage;
 }
 
 //Static function to carry out battles between two monsters
@@ -42,7 +51,7 @@ Monster& MonsterBattle::Battle(Monster& m1, Monster& m2, std::mt19937& mt)
 	//battle loop. Ends when either monster dies
 	while (!m1.isDead() && !m2.isDead())
 	{
-		if (turn == 1 && m1TurnCnt == 0)
+		if (turn == 1 && m1TurnCnt == 0) //First turn and m1 hasn't attacked yet (m2 went first)
 		{
 			m1.attack(m2, mt);
 			m1TurnCnt++;
@@ -51,7 +60,7 @@ Monster& MonsterBattle::Battle(Monster& m1, Monster& m2, std::mt19937& mt)
 			turn++;
 			continue;
 		}
-		else if (turn == 1 && m2TurnCnt == 0)
+		else if (turn == 1 && m2TurnCnt == 0) //First turn and m2 hasn't attacked yet (m1 went first)
 		{
 			m2.attack(m1, mt);
 			m2TurnCnt++;
@@ -60,7 +69,7 @@ Monster& MonsterBattle::Battle(Monster& m1, Monster& m2, std::mt19937& mt)
 			turn++;
 			continue;
 		}
-		else if (m1TurnCnt < m2TurnCnt)
+		else if (m1TurnCnt < m2TurnCnt) //If m2 went first, m1 will be attacking first for the rest of the battle
 		{
 			m1.attack(m2, mt);
 			m1TurnCnt++;
@@ -69,7 +78,7 @@ Monster& MonsterBattle::Battle(Monster& m1, Monster& m2, std::mt19937& mt)
 			turn++;
 			continue;
 		}
-		else
+		else //If m1 went first, m2 will be attacking first for the rest of the battle
 		{
 			m2.attack(m1, mt);
 			m2TurnCnt++;
@@ -80,6 +89,9 @@ Monster& MonsterBattle::Battle(Monster& m1, Monster& m2, std::mt19937& mt)
 		}
 
 	}
+
+	//Print the winner of the battle
+	MonsterBattle::printWinner(m1, m2);
 
 	return (m1.isDead()) ? m2 : m1;
 }
